d1daab28cb8080ed4fa6d77e1e04cd2a5a7ea4df
Bagg3 <andreasbaggenielsen@gmail.com>
2023-12-01 13:27:13 +0100

    Working edition, problem was with adding to char

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -25,36 +25,42 @@ int main()
 
     int sum = 0;
 
+    // Loop through all lines
     for (auto line : lines)
     {
         std::string numComb;
+        char firstNum = '\0';
+        char lastNum = '\0';
+        bool firstNumFound = false;
+
+        // Loop through all chars in line
         for (size_t i = 0; i < line.size(); i++)
         {
-            char firstNum, secNum;
-            bool firstNumFound = false;
+            // Check if first number is found
             if (!firstNumFound)
             {
+                // When first number is found, set firstNum and lastNum to that number
                 if (line[i] >= '0' && line[i] <= '9')
                 {
                     firstNum = line[i];
-                    secNum = line[i];
+                    lastNum = line[i];
                     firstNumFound = true;
                 }
             }
-            else if (line[i] < '0' || line[i] > '9')
-            {
-                char secNum = line[i];
-            }
 
-            if (i == line.size() - 1)
+            // Check if last number is equal to the first number
+            if (line[i] >= '0' && line[i] <= '9')
             {
-                if (firstNum == secNum)
-                {
-                    numComb += firstNum;
-                }
+                lastNum = line[i];
             }
         }
+        // Convert chars to string and then to int
+        numComb = firstNum;
+        numComb += lastNum;
+        sum += std::stoi(numComb);
     }
+
+    std::cout << "Sum: " << sum << std::endl;
 
     return 0;
 }
